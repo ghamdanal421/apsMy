709b158a0e695d154c57fe486c79b7fa962b0cb4
ghamdanal421 <46650498+ghamdanal421@users.noreply.github.com>
2019-12-01 18:02:38 +0300

    Update apsmy.js

--- apsmy.js
+++ apsmy.js
@@ -1,5 +1,4 @@
 
-<script>
 
 
 var myAPS = '<style>.elements-promo{overflow:hidden;width:728px;max-width:100%;max-height:90px;background-color:#f93d66;background:linear-gradient(145deg,#5be0e0,#642591);border-radius:4px}.elements{overflow:hidden;float:right;color:#FFF;padding-right:20px}img.elements-promo__image{float:left;width:22%;overflow:hidden}h2.elements-heading{margin:0;margin-top:15px}p.elements-heading{color:#e4e2e2}</style><div class="elements-promo"><div class="elements-promo"><div class="elements"><h2 class="elements-heading">أنشاء موقع معنا </h2><p class="elements-heading">فريق الاحترافي محترفون و متخصصون في تصميم المواقع على منصة بلوجر </p></div><img aria-hidden="true" class="elements-promo__image" src="https://www.webspotlight.in/images/banner-graphic.svg" alt="Girl with headphones"></div></div>';
@@ -8,4 +7,3 @@ var myAPS = '<style>.elements-promo{overflow:hidden;width:728px;max-width:100%;m
 
 
 console.log('good');
-</script>
